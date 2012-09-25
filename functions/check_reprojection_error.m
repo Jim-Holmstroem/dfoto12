0e3ec5efc8a8148b9b643c15f8569b7263bf6240
Jim Holmström <jim.holmstroem@gmail.com>
2012-09-26 00:18:24 +0200

    made reprojection error check, not tested

--- functions/check_reprojection_error.m
+++ functions/check_reprojection_error.m
@@ -12,9 +12,13 @@
 %      
 
 function [error_average, error_max] = check_reprojection_error(data, cam, model)
+    reproj_data = cam*model;
+    
+    for i = 1:3:size(cam,1) %fugly but have 2 do it
+        reproj_data_norm(i:(i+2),:) = norm_points_to_one(reproj_data(i:(i+2),:);
+    end
 
-%------------------------------
-%
-% FILL IN THIS PART
-%
-%------------------------------
+    err = data - reproj_data_norm;
+    error_average = mean(mean(abs(err)));
+    error_max = max(max(abs(err)));
+
